fdf538d95ef630643e806ebf54b4f53a4c108960
grsmasher <grsmasher@cf734d27-4843-9458-1789-c035f951c043>
2010-06-07 23:18:48 +0000

    especificado método AG1.hayChoque

--- trunk/Evolucionae/Form1.cs
+++ trunk/Evolucionae/Form1.cs
@@ -43,7 +43,7 @@ namespace Evolucionae
             cargarComboPersonas();
 
             //imprimirListaPersonas();
-            //this.pruebaTonta();
+            this.pruebaTonta();
             this.inicializarGridHorario();
         }
 
